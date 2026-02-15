8a501d8a946328043492e1f8b70a54d5978bbd9a
Emeka Anyaegbunam <chukwuemekamusic@gmail.com>
2026-02-15 08:50:02 +0000

    contract: interaction script

--- contracts/script/interactions/Oracle.s.sol
+++ contracts/script/interactions/Oracle.s.sol
@@ -6,7 +6,7 @@ import {console} from "forge-std/console.sol";
 import {IOracleRouter} from "src/interfaces/IOracleRouter.sol";
 import {Constants} from "script/Constants.s.sol";
 
-contract getOracleConfigWETH is DeploymentHelper {
+contract getOracleConfig is DeploymentHelper {
     function run() external view {
         // Load core deployment
         CoreDeployment memory deployment = loadDeployment();
@@ -15,13 +15,21 @@ contract getOracleConfigWETH is DeploymentHelper {
         IOracleRouter oracleRouter = IOracleRouter(deployment.oracleRouter);
 
         // Get the oracle config
-        IOracleRouter.OracleConfig memory config = oracleRouter.getOracleConfig(Constants.WETH_BASE_S);
+        IOracleRouter.OracleConfig memory configWETH = oracleRouter.getOracleConfig(Constants.WETH_BASE_S);
+        IOracleRouter.OracleConfig memory configUSDC = oracleRouter.getOracleConfig(Constants.USDC_BASE_S);
 
         console.log("Oracle Config for:", Constants.WETH_BASE_S);
-        console.log("Chainlink Feed:", config.chainlinkFeed);
-        console.log("Uniswap Pool:", config.uniswapPool);
-        console.log("TWAP Window:", config.twapWindow);
-        console.log("Max Staleness:", config.maxStaleness);
-        console.log("Is Token0:", config.isToken0);
+        console.log("Chainlink Feed:", configWETH.chainlinkFeed);
+        console.log("Uniswap Pool:", configWETH.uniswapPool);
+        console.log("TWAP Window:", configWETH.twapWindow);
+        console.log("Max Staleness:", configWETH.maxStaleness);
+        console.log("Is Token0:", configWETH.isToken0);
+
+        console.log("\nOracle Config for:", Constants.USDC_BASE_S);
+        console.log("Chainlink Feed:", configUSDC.chainlinkFeed);
+        console.log("Uniswap Pool:", configUSDC.uniswapPool);
+        console.log("TWAP Window:", configUSDC.twapWindow);
+        console.log("Max Staleness:", configUSDC.maxStaleness);
+        console.log("Is Token0:", configUSDC.isToken0);
     }
 }
